9cf3fe50e36dfb49dcbb21bea120b99f3bc14b30
Julian Mahecha <jmahecha@belatrixsf.com>
2019-02-05 11:17:37 -0500

    assigment 1

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,96 +1,46 @@
 
-var deferredPrompt;
+var button = document.querySelector('#start-button');
+var output = document.querySelector('#output');
 
-if (!window.Promise) {
-  window.Promise = Promise;
-}
+button.addEventListener('click', function() {
+  // Create a new Promise here and use setTimeout inside the function you pass to the constructor
 
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker
-    .register('/sw.js')
-    .then(function () {
-      console.log('Service worker registered!');
-    })
-    .catch(function(err) {
-      console.log(err);
+  var promise = new Promise(function(resolve, reject){
+    setTimeout(function() {
+      resolve('https://swapi.co/api/people/1')
+    }, 3000);
+  })
+
+  promise.then(function(url){
+    return fetch(url);
+  }).then(function(response){
+    return response.json();
+  }).then(function(data){
+    output.append(data.name);
+  })
+
+  var promise2 = new Promise(function(resolve, reject){
+    setTimeout(function() {
+      resolve('https://httpbin.org/put')
+    }, 3000);
+  })
+  promise2.then(function(url){
+    return fetch(url,{
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      },
+      mode: 'cors',
+      body: JSON.stringify({person: {name: 'Max', age: 28}})
     });
-}
-
-window.addEventListener('beforeinstallprompt', function(event) {
-  console.log('beforeinstallprompt fired');
-  event.preventDefault();
-  deferredPrompt = event;
-  return false;
-});
-
-var promise = new Promise(function(resolve, reject) {
-  setTimeout(function() {
-    //resolve('This is executed once the timer is done!');
-    reject({code: 500, message: 'An error occurred!'});
-    //console.log('This is executed once the timer is done!');
-  }, 3000);
-});
-
-var xhr = new XMLHttpRequest();
-xhr.open('GET', 'https://httpbin.org/ip');
-xhr.responseType = 'json';
-
-xhr.onload = function() {
-  console.log(xhr.response);
-};
-
-xhr.onerror = function() {
-  console.log('Error!');
-};
-
-xhr.send();
-
-fetch('https://httpbin.org/ip')
-  .then(function(response) {
-    console.log(response);
+  }).then(function(response){
     return response.json();
-  })
-  .then(function(data) {
-    console.log(data);
+  }).then(function(data){
+    output.append(data.json.person.name);
   })
   .catch(function(err) {
     console.log(err);
   });
 
-fetch('https://httpbin.org/post', {
-  method: 'POST',
-  headers: {
-    'Content-Type': 'application/json',
-    'Accept': 'application/json'
-  },
-  mode: 'cors',
-  body: JSON.stringify({message: 'Does this work?'})
-})
-  .then(function(response) {
-    console.log(response);
-    return response.json();
-  })
-  .then(function(data) {
-    console.log(data);
-  })
-  .catch(function(err) {
-    console.log(err);
-  });
-
-// promise.then(function(text) {
-//   return text;
-// }, function(err) {
-//   console.log(err.code, err.message)
-// }).then(function(newText) {
-//   console.log(newText);
-// });
-
-promise.then(function(text) {
-  return text;
-}).then(function(newText) {
-  console.log(newText);
-}).catch(function(err) {
-  console.log(err.code, err.message);
 });
-
-console.log('This is executed right after setTimeout()');
