fd25b493631454dde350362ff30d361d074e5f93
Julian Mahecha <jmahecha@H5740.ad.hugeinc.com>
2019-03-19 15:24:13 -0500

    basic notifications

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,5 +1,7 @@
 
 var deferredPrompt;
+var enableNotificationsButtons = document.querySelectorAll('.enable-notifications');
+
 
 if (!window.Promise) {
   window.Promise = Promise;
@@ -22,3 +24,42 @@ window.addEventListener('beforeinstallprompt', function(event) {
   deferredPrompt = event;
   return false;
 });
+
+function displayConfirmNotification(){
+  if('serviceWorker' in navigator){
+    var options = {
+      body:'You successfully subscribed to our Notification service!!',
+      icon: '/src/images/icons/app-icon-96x96.png',
+      image:'/src/images/sf-boat.jpg',
+      dir:'ltr',
+      lang:'en-US', // BCP 47
+      vibrate: [100, 50, 200],
+      badge: '/src/images/icons/app-icon-96x96.png',
+      tag: 'confirm-notification',
+      renotify: true
+    };
+    navigator.serviceWorker.ready
+    .then(function(swreg){
+      swreg.showNotification('Successfully subscribed!! (From SW)', options);
+    });
+  }
+}
+
+function askForNotificacionPermission(){
+  Notification.requestPermission(function(result){
+    console.log('User Choise', result)
+    if(result !== 'granted'){
+      console.log('No notification permission granted!!');
+    } else {
+      displayConfirmNotification()
+    }
+  });
+}
+
+if('Notification' in window){
+  for(var i  = 0 ; i < enableNotificationsButtons.length ; i++){
+    enableNotificationsButtons[i].style.display = 'inline-block';
+    enableNotificationsButtons[i].addEventListener('click', askForNotificacionPermission)
+
+  }
+}
