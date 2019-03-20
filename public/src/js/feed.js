cb40baded5751652b240bdee00b253a1a7c78848
Julian Mahecha <jmahecha@H5740.ad.hugeinc.com>
2019-03-20 14:05:41 -0500

    add camera and select file to post

--- public/src/js/feed.js
+++ public/src/js/feed.js
@@ -5,12 +5,64 @@ var sharedMomentsArea = document.querySelector('#shared-moments');
 var form = document.querySelector('form');
 var titleInput = document.querySelector('#title');
 var locationInput = document.querySelector('#location');
+var videoPlayer = document.querySelector('#player');
+var canvasElement = document.querySelector('#canvas');
+var captureButton = document.querySelector('#capture-btn');
+var imagePicker = document.querySelector('#image-picker');
+var imagePickerArea = document.querySelector('#pick-image');
+var picture;
+
+function initializeMedia() {
+  if (!('mediaDevices' in navigator)) {
+    navigator.mediaDevices = {};
+  }
+
+  if (!('getUserMedia' in navigator.mediaDevices)) {
+    navigator.mediaDevices.getUserMedia = function(constraints) {
+      var getUserMedia = navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+
+      if (!getUserMedia) {
+        return Promise.reject(new Error('getUserMedia is not implemented!'));
+      }
+
+      return new Promise(function(resolve, reject) {
+        getUserMedia.call(navigator, constraints, resolve, reject);
+      });
+    }
+  }
+
+  navigator.mediaDevices.getUserMedia({video: true})
+    .then(function(stream) {
+      videoPlayer.srcObject = stream;
+      videoPlayer.style.display = 'block';
+    })
+    .catch(function(err) {
+      imagePickerArea.style.display = 'block';
+    });
+}
+
+captureButton.addEventListener('click', function(event) {
+  canvasElement.style.display = 'block';
+  videoPlayer.style.display = 'none';
+  captureButton.style.display = 'none';
+  var context = canvasElement.getContext('2d');
+  context.drawImage(videoPlayer, 0, 0, canvas.width, videoPlayer.videoHeight / (videoPlayer.videoWidth / canvas.width));
+  videoPlayer.srcObject.getVideoTracks().forEach(function(track) {
+    track.stop();
+  });
+  picture = dataURItoBlob(canvasElement.toDataURL());
+});
+
+imagePicker.addEventListener('change', function(event) {
+  picture = event.target.files[0];
+});
 
 function openCreatePostModal() {
-  
-  createPostArea.style.transform = 'translateY(0)';
-  
-  
+  // createPostArea.style.display = 'block';
+  // setTimeout(function() {
+    createPostArea.style.transform = 'translateY(0)';
+    initializeMedia();
+  // }, 1);
   if (deferredPrompt) {
     deferredPrompt.prompt();
 
@@ -38,8 +90,11 @@ function openCreatePostModal() {
 }
 
 function closeCreatePostModal() {
-  //createPostArea.style.display = 'none';
   createPostArea.style.transform = 'translateY(100vh)';
+  imagePickerArea.style.display = 'none';
+  videoPlayer.style.display = 'none';
+  canvasElement.style.display = 'none';
+  // createPostArea.style.display = 'none';
 }
 
 shareImageButton.addEventListener('click', openCreatePostModal);
@@ -71,7 +126,6 @@ function createCard(data) {
   cardTitle.className = 'mdl-card__title';
   cardTitle.style.backgroundImage = 'url(' + data.image + ')';
   cardTitle.style.backgroundSize = 'cover';
-  cardTitle.style.backgroundPosition = 'bottom';
   cardWrapper.appendChild(cardTitle);
   var cardTitleTextElement = document.createElement('h2');
   cardTitleTextElement.style.color = 'white';
@@ -125,54 +179,57 @@ if ('indexedDB' in window) {
     });
 }
 
-function sendData(){
+function sendData() {
+  var id = new Date().toISOString();
+  var postData = new FormData();
+  postData.append('id', id);
+  postData.append('title', titleInput.value);
+  postData.append('location', locationInput.value);
+  postData.append('file', picture, id + '.png');
+
   fetch('https://us-central1-pwa-course-b0d4d.cloudfunctions.net/storePostData', {
     method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      'Accept': 'applicatoin/json'
-    },
-    body: JSON.stringify({
-      id: new Date().toISOString(),
-      title: titleInput.value,
-      location: locationInput.value,
-      image: "https://firebasestorage.googleapis.com/v0/b/pwa-course-b0d4d.appspot.com/o/sf-boat.jpg?alt=media&token=8fcef859-cac0-4d41-94e0-cea8247ee85e"
+    body: postData
+  })
+    .then(function(res) {
+      console.log('Sent data', res);
+      updateUI();
     })
-  })
-  .then(function(res){
-    console.log('Send data', res);
-    updateUI();
-  })
 }
 
-form.addEventListener('submit', function(event){
+form.addEventListener('submit', function(event) {
   event.preventDefault();
 
-  if(titleInput.value.trim() === '' || locationInput.value.trim() === ''){
-    alert('Please enter valid data!')
-    return; 
+  if (titleInput.value.trim() === '' || locationInput.value.trim() === '') {
+    alert('Please enter valid data!');
+    return;
   }
 
   closeCreatePostModal();
 
-  if('serviceWorker' in navigator && 'SyncManager' in window){
+  if ('serviceWorker' in navigator && 'SyncManager' in window) {
     navigator.serviceWorker.ready
-    .then(function(sw){
-      var post = {
-        id: new Date().toISOString(),
-        title: titleInput.value,
-        location: locationInput.value
-      }
-      writeData('sync-posts', post)
-      .then(function(){
-        return sw.sync.register('sync-new-posts');
-      })
-      .then(function( ){})
-        var snackbarContainer = document.querySelector('#confirmation-toast');
-        var data = {message : 'Your post was saved for syncing!!'};
-        snackbarContainer.MaterialSnackbar.showSnackbar(data);
-    })
+      .then(function(sw) {
+        var post = {
+          id: new Date().toISOString(),
+          title: titleInput.value,
+          location: locationInput.value,
+          picture: picture
+        };
+        writeData('sync-posts', post)
+          .then(function() {
+            return sw.sync.register('sync-new-posts');
+          })
+          .then(function() {
+            var snackbarContainer = document.querySelector('#confirmation-toast');
+            var data = {message: 'Your Post was saved for syncing!'};
+            snackbarContainer.MaterialSnackbar.showSnackbar(data);
+          })
+          .catch(function(err) {
+            console.log(err);
+          });
+      });
   } else {
     sendData();
   }
-})
+});
